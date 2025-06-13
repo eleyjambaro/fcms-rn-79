c4a01110af7954915b22a1a7f3705aca97e061fd
LA Jambaro <eleyjambaro@gmail.com>
2025-06-13 10:17:40 +0800

    Adjust vertical alignment of Permission, Create account, Login screens

--- src/screens/FilesAndMediaReadAndWritePermissionNeeded.js
+++ src/screens/FilesAndMediaReadAndWritePermissionNeeded.js
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    paddingTop: 60,
+    paddingTop: 100,
     paddingBottom: 50,
     justifyContent: 'center',
   },
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -174,7 +174,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    paddingTop: 80,
+    paddingTop: 100,
     paddingBottom: 50,
     justifyContent: 'center',
   },
