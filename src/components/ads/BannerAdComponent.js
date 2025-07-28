53bf443307f3007fdadf34346f2df847d20dec2f
LA Jambaro <eleyjambaro@gmail.com>
2025-07-29 01:11:05 +0800

    Add advertising ID permission

--- src/components/ads/BannerAdComponent.js
+++ src/components/ads/BannerAdComponent.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
-import appJson from '../../../app.json';
 
 const adUnitId = __DEV__
   ? TestIds.ADAPTIVE_BANNER
-  : appJson['react-native-google-mobile-ads'].android_app_id;
+  : 'ca-app-pub-1258147525251553/7620972083';
 
 const BannerAdComponent = () => {
   return (
