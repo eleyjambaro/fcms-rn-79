9d228e2a523797b2449ef14ed2482148bd0101cc
LA Jambaro <eleyjambaro@gmail.com>
2026-06-10 15:04:44 +0800

    Hide ads for licensed users

--- src/components/ads/BannerAdComponent.js
+++ src/components/ads/BannerAdComponent.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {useQuery} from '@tanstack/react-query';
 import {BannerAd, BannerAdSize} from 'react-native-google-mobile-ads';
+
+import {getLicenseStatus} from '../../localDbQueries/license';
 
 const BannerAdComponent = props => {
   const {unitId} = props;
+
+  const {data: licenseStatusData} = useQuery(
+    ['licenseKeyStatus', {}],
+    getLicenseStatus,
+  );
+
+  const licenseStatus = licenseStatusData?.result;
+  const hasActiveLicense =
+    licenseStatus?.hasLicenseToken && !licenseStatus?.isLicenseExpired;
+
+  // Hide ads for users with an active (non-expired) license.
+  if (hasActiveLicense) {
+    return null;
+  }
 
   return (
     <BannerAd unitId={unitId} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
